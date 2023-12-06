8c835e1185afeef3854c9ff754daddd99b38b056
Giacom0Mancuso <giacomomancuso35@email.com>
2023-12-06 17:21:51 +0100

    work done

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,2 +1,33 @@
-package org.lessons.java.geometria;public class Rettangolo {
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+        int altezza;
+        int base;
+        int perimetro;
+        int area;
+
+
+        Rettangolo(){
+            this.altezza = altezza;
+            this.base = base;
+        }
+
+        void calcolaPerimetro(int altezza, int base){
+            perimetro = (altezza + base) * 2;
+        }
+
+        void calcolaArea(int altezza, int base){
+            area = base * altezza;
+        }
+
+        String getInfoPerimetro(){
+            String info = "Perimetro: " + perimetro;
+            return info;
+        }
+        String getInfoArea(){
+        String info = "Area: " + area;
+        return info;
+        }
+        
 }
+
--- src/org/lessons/java/geometria/main.java
+++ src/org/lessons/java/geometria/main.java
@@ -1,2 +1,20 @@
-package org.lessons.java.geometria;public class main {
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Rettangolo rettangolo = new Rettangolo();
+        int altezza = Integer.parseInt(scanner.nextLine());
+        System.out.println("Altezza: " + altezza);
+        int base = Integer.parseInt(scanner.nextLine());
+        System.out.println("Base: " + base);
+
+        rettangolo.calcolaPerimetro(altezza, base);
+        rettangolo.calcolaArea(altezza, base);
+        System.out.println(rettangolo.getInfoPerimetro());
+        System.out.println(rettangolo.getInfoArea());
+    }
 }
